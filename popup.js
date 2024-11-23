e427799aa62abf78ac5183beb417ed6cf9f7bee4
Markkyu <103357154+Markkyu@users.noreply.github.com>
2024-11-24 01:06:30 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,25 @@
 const container = document.querySelector('.container')
+const add_deck = document.querySelector('.add-deck')
 
-document.querySelector('#open-popup').addEventListener('click', function (){
+// Activate pop-up event via button #open-popup and #open-popup2
+document.querySelector('#open-popup2').addEventListener('click', () =>{
     document.body.classList.add('active-popup');
     // document.main.classList.add('noClick');
 });
 
-document.querySelector('.popup .close-btn').addEventListener('click', function (){
+document.querySelector('#open-popup').addEventListener('click', () =>{
+    document.body.classList.add('active-popup');
+    // document.main.classList.add('noClick');
+});
+
+
+// Deactivate pop-up event via button .close-btn and .cancel-deck
+document.querySelector('.popup .close-btn').addEventListener('click', () =>{
     document.body.classList.remove('active-popup');
-    container.classList.remove('noClick');
+    // container.classList.remove('noClick');
+});
+
+document.querySelector('.cancel-deck').addEventListener('click', () =>{
+    document.body.classList.remove('active-popup');
+    // container.classList.remove('noClick');
 });
